e7251765c995da00261d1af9fa20dfda27ff5816
neithalrajpurohit <rajpurohitneithal@gmail.com>
2022-11-04 18:27:59 +0530

    Initial_commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.html"],
+  theme: {
+    extend: {
+      screens: {
+        980: "980px",
+        1080: "1080px",
+      },
+    },
+  },
+  plugins: [],
+};
